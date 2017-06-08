5eaa666c4090c56a3f039699b5b5af5dcfddecc9
Chisoo <chisoo@mac.com>
2017-06-08 17:39:35 +0000

    updated post request to store ticker name

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+app.vars = {}
+
 @app.route('/')
 def main():
   return redirect('/index')
@@ -11,7 +13,13 @@ def index():
 	if request.method == 'GET':
 		return render_template('index.html')
 	else: 
-		return 'request.method was not a GET!'
+		app.vars['ticker_name'] = request.form['ticker']
+
+		f.open('%s.txt'%(app.vars['ticker_name']), 'w')
+		f.write('Ticker Name: %s\n'%(app.vars['ticker_name']))
+		f.close()
+
+		return 'requested data on %s'%(app.vars['ticker_name'])
 
 if __name__ == '__main__':
 	app.run(port=33507)
