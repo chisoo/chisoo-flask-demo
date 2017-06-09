3b651b2eb267b0921f01eb4ff07342728e012e18
Chisoo <chisoo@mac.com>
2017-06-09 21:45:09 +0000

    Updated dictionary to have tuples

--- app.py
+++ app.py
@@ -50,8 +50,9 @@ def index():
 		# plot.multi_line(xs = [quandl_df.index.values] * num_lines, 
 		# 	ys = [quandl_df[col].values for col in quandl_df], 
 		# 	line_color = mypalette, line_width = 5)
-		data = dict(CLOSE = quandl_df['Close'], Date = quandl_df['Date'])
-		plot = TimeSeries(data, \
+		data = dict([('CLOSE', quandl_df['Close']), ('Date', quandl_df['Date'])])
+
+		plot = TimeSeries(data, x = 'Date', \
 			title = 'plot title placeholder', ylabel = 'Stock Prices')
 
 		script, div = components(plot)
