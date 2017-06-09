c40f1fef0706d95c0b3b2e1b697ee8316eeb57ab
Chisoo <chisoo@mac.com>
2017-06-09 04:39:38 +0000

    Deleted get_quandl_data.py

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ def index():
 		return render_template('index.html')
 	else: 
 		app.vars['ticker_name'] = request.form['ticker']
+
+		# quandl.ApiConfig.api_key = os.environ.get('QUANDL_API_KEY')
+		# ticker_name = 'AAPL'
+		# quandl_tag = 'WIKI/' + ticker_name
+		# quandl_data = quandl.get(quandl_tag)
+
+		# # keep only necessary columns
+		# quandl_data = quandl_data[['Close', 'Adj. Close', 'Open', 'Adj. Open']]
+		# quandl_data.reset_index(inplace = True)
+
+		# # keep only the last month data
+		# start_date = quandl_data['Date'].max() - timedelta(days = 30)
+		# quandl_data = quandl_data[quandl_data['Date'] >= start_date]
+		# quandl_data.reset_index(drop = True, inplace = True)
+
+		# print(quandl_data)
 		print('test')
 
 		return "Requested Data"
